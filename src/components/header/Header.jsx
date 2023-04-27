adf02314482ffcb0736717966e2135c6addd4ef5
aishwary2994 <yash.yadav29@gmail.com>
2023-04-27 17:14:15 +0530

    fixer2.0

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello i am</h5>
         <h1>Aishwary Yadav</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">MERN Stack Developer</h5>
         <CTA />
         <HeaderSocials />
 
