7b9dff1746b82939701e186f5aefb4477c869170
aishwary2994 <yash.yadav29@gmail.com>
2023-05-15 11:39:11 +0530

    resume pdf

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div>
       <div className="cta">
-        <a href='https://resume.io/r/aAa1ycERW' target='_blank' className='btn'>View CV</a>
+        <a href='https://drive.google.com/file/d/1sBzb7d8E_ZO5glZjjgaRkLrqFv6nNxJw/view?usp=share_link' target='_blank' className='btn'>View CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
       </div>
     </div>
